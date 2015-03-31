dc92aade339b5c017fe224ec6d489a9cb9b56991
Leon Mergen <leon@solatis.com>
2015-03-31 16:36:43 +0700

    Initial BTC API wrapper

---
+++ bitgloom-btc/src/Bitgloom/BTC.hs
@@ -0,0 +1,45 @@
+module Bitgloom.BTC where
+
+import Control.Monad.IO.Class
+import Control.Concurrent (forkIO, killThread)
+import Control.Concurrent.MVar
+import Control.Monad.Catch (handle)
+
+import qualified Data.ByteString as BS
+
+import Network.HTTP.Client (HttpException (..))
+import Network.HTTP.Types (Status)
+
+import Network.Bitcoin ( getClient
+                       , getBitcoindInfo)
+
+data Availability =
+  Available |
+  ConnectionRefused |
+  IncorrectPort |
+  Unauthorized
+  deriving (Show, Eq)
+
+isAvailable :: MonadIO m => String -> Int -> BS.ByteString -> BS.ByteString -> m Availability
+isAvailable host port user pass =
+  let performTest errMsg = do
+        client <- getClient ("http://" ++ host ++ ":" ++ (show port)) user pass
+        _      <- getBitcoindInfo client
+        putMVar errMsg Available
+
+
+      handleExceptions errMsg (FailedConnectionException2 _ _ _ _) = putMVar errMsg ConnectionRefused
+      handleExceptions errMsg (StatusCodeException statusUnauthorized _ _) = putMVar errMsg Unauthorized
+
+  in do
+    errMsg <- liftIO $ newEmptyMVar
+
+    threadId <- liftIO $ forkIO $
+      handle
+        (handleExceptions errMsg)
+        (performTest errMsg)
+
+    res <- liftIO $ takeMVar errMsg
+    liftIO $ killThread threadId
+
+    return res
---
+++ bitgloom-btc/test/Bitgloom/BTCSpec.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Bitgloom.BTCSpec where
+
+import Bitgloom.BTC
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "when testing service availability" $ do
+    it "should work when providing SAM host/port" $ do
+      isAvailable "127.0.0.1" 18332 "user" "pass" `shouldReturn` Available
+    it "should return error when providing non-existing port" $
+      isAvailable "127.0.0.1" 1234  "user" "pass" `shouldReturn` ConnectionRefused
+    it "should return error when providing invalid authentication info" $
+      isAvailable "127.0.0.1" 18332  "nouser" "nopass" `shouldReturn` Unauthorized
