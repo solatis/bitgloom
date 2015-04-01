6fa52b718edc5f4ebd70411475c03fddb2f8ef51
Leon Mergen <leon@solatis.com>
2015-04-01 14:54:24 +0700

    Fixes typo in test case

--- bitgloom-btc/test/Bitgloom/BTCSpec.hs
+++ bitgloom-btc/test/Bitgloom/BTCSpec.hs
@@ -8,7 +8,7 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "when testing service availability" $ do
-    it "should work when providing SAM host/port" $ do
+    it "should work when providing RPC host/port" $ do
       isAvailable "127.0.0.1" 18332 "user" "pass" `shouldReturn` Available
     it "should return error when providing non-existing port" $
       isAvailable "127.0.0.1" 1234  "user" "pass" `shouldReturn` ConnectionRefused
