0f5ed331eb8612fd94158dbfeea9b1b2b1b29258
Leon Mergen <leon@solatis.com>
2015-04-05 14:38:13 +0700

    Now using real Yesod Forms with CSRF token
    Increased security by now using Yesod's forms instead of a custom one,
    which makes use of a CSRF token.

--- bitgloom-www/Handler/Configuration.hs
+++ bitgloom-www/Handler/Configuration.hs
@@ -2,7 +2,8 @@ module Handler.Configuration where
 
 import Import hiding (update)
 
-import qualified Data.Text as T (pack, unpack, concat)
+import Data.Maybe (fromMaybe)
+import qualified Data.Text as T (pack, unpack, concat, empty)
 import qualified Data.Text.Encoding as TE (decodeUtf8, encodeUtf8)
 
 import Data.Attoparsec.Text (char, decimal, skipSpace, endOfInput, parseOnly)
@@ -14,25 +15,46 @@ getConfigurationR = do
   master <- getYesod
   config <- runDB retrieve
 
+  (formWidget, formEnctype) <- generateFormPost (configurationForm config)
+
   defaultLayout $ do
     setTitle "Configuration"
     $(widgetFile "configuration")
 
 postConfigurationR :: Handler Html
 postConfigurationR = do
-  master <- getYesod
+  config <- runDB retrieve
+  ((result, widget), enctype) <- runFormPost (configurationForm config)
 
-  config <- runInputPost $ Configuration
-            <$> ireq textField "i2pTcpHost"
-            <*> ireq intField "i2pTcpPort"
-            <*> ireq textField "i2pUdpHost"
-            <*> ireq intField "i2pUdpPort"
-            <*> ireq textField "btcHost"
-            <*> ireq intField "btcPort"
-            <*> ireq textField "btcUsername"
-            <*> ireq textField "btcPassword"
+  case result of
+   FormSuccess config -> do
+     runDB $ store config
+     setMessage "Configuration has been stored"
+     redirect ConfigurationR
 
-  runDB $ store config
+   FormMissing     -> error "Not a POST request!"
+   FormFailure err -> error "Invalid form data!"
 
-  setMessage "Configuration has been stored"
-  redirect ConfigurationR
+configurationForm :: Configuration -> Html -> MForm Handler (FormResult Configuration, Widget)
+configurationForm config extra = do
+  (i2pTcpHostRes, i2pTcpHostView)   <- mreq textField "TCP host" (Just (configurationI2pTcpHost config))
+  (i2pTcpPortRes, i2pTcpPortView)   <- mreq intField "TCP port" (Just (configurationI2pTcpPort config))
+  (i2pUdpHostRes, i2pUdpHostView)   <- mreq textField "UDP host" (Just (configurationI2pUdpHost config))
+  (i2pUdpPortRes, i2pUdpPortView)   <- mreq intField "UDP port" (Just (configurationI2pUdpPort config))
+  (btcHostRes, btcHostView)         <- mreq textField "Host" (Just (configurationBtcHost config))
+  (btcPortRes, btcPortView)         <- mreq intField "Port" (Just (configurationBtcPort config))
+  (btcUsernameRes, btcUsernameView) <- mreq textField "Username" (Just (configurationBtcUsername config))
+  (btcPasswordRes, btcPasswordView) <- mreq passwordField "Password" (Just (configurationBtcPassword config))
+
+  let configurationRes = Configuration <$> i2pTcpHostRes
+                                       <*> i2pTcpPortRes
+                                       <*> i2pUdpHostRes
+                                       <*> i2pUdpPortRes
+                                       <*> btcHostRes
+                                       <*> btcPortRes
+                                       <*> btcUsernameRes
+                                       <*> btcPasswordRes
+
+  let widget = $(widgetFile "configuration-form")
+
+  return (configurationRes, widget)
