09ef1d7e245025e2fe1f32ddb3c8ffb92c488c9b
Leon Mergen <leon@solatis.com>
2015-04-05 15:00:03 +0700

    Fixes test cases

--- bitgloom-www/test/Handler/ConfigurationSpec.hs
+++ bitgloom-www/test/Handler/ConfigurationSpec.hs
@@ -4,7 +4,7 @@ import TestImport
 
 spec :: Spec
 spec = withApp $
-    it "loads the index and checks it looks right" $ do
+    it "submitting the form with working info is handled correctly" $ do
         get ConfigurationR
         statusIs 200
 
@@ -12,13 +12,16 @@ spec = withApp $
           setMethod "POST"
           setUrl ConfigurationR
           addToken
-          byLabel "TCP endpoint" "127.0.0.1:7656"
-          byLabel "UDP endpoint" "127.0.0.1:7655"
-          byLabel "Endpoint" "127.0.0.1:18332"
+          byLabel "TCP host" "127.0.0.1"
+          byLabel "TCP port" "7656"
+          byLabel "UDP host" "127.0.0.1"
+          byLabel "UDP port" "7655"
+          byLabel "Host" "127.0.0.1"
+          byLabel "Port" "18332"
           byLabel "Username" "user"
           byLabel "Password" "pass"
 
-        statusIs 200
+        statusIs 303
         -- more debugging printBody
         -- htmlCount ".message" 1
         -- htmlAllContain ".message" "Some Content"
