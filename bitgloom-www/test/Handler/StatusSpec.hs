63219a94057fa197ada0eff0ab5e2f3b7f46c72e
Leon Mergen <leon@solatis.com>
2015-04-07 08:42:11 +0700

    Validates status page ready to serve button

--- bitgloom-www/test/Handler/StatusSpec.hs
+++ bitgloom-www/test/Handler/StatusSpec.hs
@@ -4,6 +4,9 @@ import TestImport
 
 spec :: Spec
 spec = withApp $
-    it "loads the index and checks it looks right" $ do
+    it "loads the index and checks for ready to serve button" $ do
         get StatusR
         statusIs 200
+
+        htmlCount ".special" 1
+        htmlAllContain ".special" "Ready to serve"
