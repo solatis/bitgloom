24a6930c5be972bc203828617c223e63093d4372
Leon Mergen <leon@solatis.com>
2015-04-08 16:40:15 +0700

    Implements test case for picking random role

--- bitgloom-driver/src/Bitgloom/Driver.hs
+++ bitgloom-driver/src/Bitgloom/Driver.hs
@@ -1,1 +1,11 @@
 module Bitgloom.Driver where
+
+import Crypto.Random (CPRG)
+import Crypto.Number.Generate (generateBetween)
+
+data Role = Advertiser | Respondent deriving (Bounded, Enum, Show, Eq)
+
+pickRole :: CPRG a => a -> (Role, a)
+pickRole g =
+  case generateBetween g (toInteger (fromEnum (minBound :: Role))) (toInteger (fromEnum (maxBound :: Role))) of
+   (i, g') -> (toEnum (fromIntegral i), g')
--- bitgloom-driver/test/Bitgloom/DriverSpec.hs
+++ bitgloom-driver/test/Bitgloom/DriverSpec.hs
@@ -2,11 +2,30 @@
 
 module Bitgloom.DriverSpec where
 
+import Crypto.Random (  CPRG
+                      , SystemRNG
+                      , createEntropyPool
+                      , cprgCreate)
+
+import Data.List (nub)
 import Bitgloom.Driver
 import Test.Hspec
+import Test.Hspec.Expectations
 
 spec :: Spec
 spec = do
-  describe "when testing service availability" $ do
-    it "should work when providing SAM host/port" $
-      True `shouldBe` True
+  describe "when picking a role" $ do
+    it "should be pick up both roles after 10 attempts" $
+      let cprg pool       = cprgCreate pool :: SystemRNG
+          randomRoles gen =
+            role : (randomRoles g')
+
+            where
+              (role, g') = pickRole gen
+
+      in do
+         entPool <- createEntropyPool
+         let res = nub $ take 10 (randomRoles (cprg entPool))
+
+         elem Advertiser res `shouldBe` True
+         elem Respondent res `shouldBe` True
