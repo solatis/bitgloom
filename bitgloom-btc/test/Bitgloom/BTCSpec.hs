7d3083734a5b1d56d6e3e185587cb1365505266f
Leon Mergen <leon@solatis.com>
2015-06-02 10:32:38 +0700

    Implements broadcasting function and test case

--- bitgloom-btc/test/Bitgloom/BTCSpec.hs
+++ bitgloom-btc/test/Bitgloom/BTCSpec.hs
@@ -2,15 +2,18 @@
 
 module Bitgloom.BTCSpec where
 
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as TE
-import qualified Data.Base58String as B58S (toText)
+import qualified Data.Base32String.Default  as B32S (fromText, toBytes)
+import qualified Data.ByteString.Char8      as BS8
+import qualified Data.Text                  as T
+import qualified Data.Text.Encoding         as TE
+import qualified Data.HexString             as HS
 
-import qualified Network.Bitcoin.Api.Client                   as Btc
-import qualified Network.Bitcoin.Api.Mining                   as Btc
+import qualified Network.Bitcoin.Api.Client as Btc
+import qualified Network.Bitcoin.Api.Mining as Btc
 
-import Bitgloom.BTC
-import Test.Hspec
+import Data.Word (Word16)
+import           Bitgloom.BTC
+import           Test.Hspec
 
 spec :: Spec
 spec = do
@@ -24,20 +27,18 @@ spec = do
     it "should return error when providing the wrong port" $
       isAvailable "127.0.0.1" 7656  "user" "pass" `shouldReturn` IncorrectPort
 
-  describe "when creating a session" $ do
-    it "can look up accounts" $ do
-      result <- Btc.withClient "127.0.0.1" 18332 "user" "pass" listAccounts
-      length (result) `shouldSatisfy` (> 0)
-
-    it "can create a new address" $ do
-      address <- Btc.withClient "127.0.0.1" 18332 "user" "pass" createAddress
-
-      T.length (B58S.toText address) `shouldSatisfy` (>= 27)
-      T.length (B58S.toText address) `shouldSatisfy` (<= 34)
-
+  describe "when testing service" $ do
     it "can broadcast a message to an address" $ do
       Btc.withClient "127.0.0.1" 18332 "user" "pass" $ \client -> do
         -- Let's flush any transactions so we know for sure we have money to spend
-        _ <- Btc.generate client 10
-        _ <- broadcast client 0.0001 (TE.encodeUtf8 (T.pack "hello world"))
+        _   <- Btc.generate client 1
+        txid <- broadcast client 0.0001 message
+        putStrLn ("txid = " ++ show txid)
         True `shouldBe` True
+
+        where
+          header = HS.toBytes $ HS.hexString "b76a"
+
+          message = header `BS8.append` onionAddress
+
+          onionAddress = B32S.toBytes $ B32S.fromText "6MQF77PTMIV3BL6S"
