4ce7ea3e1ea5ef3d36689292192bb4e898353d68
Leon Mergen <leon@solatis.com>
2015-06-05 12:16:03 +0700

    Implements 'discover' function with test cases

--- bitgloom-btc/src/Bitgloom/BTC.hs
+++ bitgloom-btc/src/Bitgloom/BTC.hs
@@ -3,7 +3,7 @@ module Bitgloom.BTC ( Availability (..)
                     , advertise
                     , discover ) where
 
-import           Control.Lens                                 ((^.), (&), (<>~))
+import           Control.Lens                                 ((&), (<>~), (^.))
 
 import           Control.Concurrent                           (forkIO,
                                                                killThread)
@@ -13,18 +13,28 @@ import           Control.Monad.Catch                          (handle)
 import           Control.Monad.IO.Class
 
 import qualified Data.ByteString                              as BS
+import           Data.Conduit                                 (($$), ($=))
+import qualified Data.Conduit                                 as C (Conduit,
+                                                                    Source)
+import Data.Maybe (fromJust, isJust)
+import qualified Data.Conduit.Combinators                     as CC (filter, map)
+import           Data.Monoid                                  ((<>))
 import qualified Data.Text                                    as T (Text)
 
 import           Network.HTTP.Client                          (HttpException (..))
 
+import qualified Data.Bitcoin.Script                          as Btc
 import qualified Data.Bitcoin.Transaction                     as Btc
 import qualified Data.Bitcoin.Types                           as Btc
-import qualified Data.Bitcoin.Script                          as Btc
+
+import qualified Control.Bitcoin.Api.Transaction              as Btc
 import qualified Network.Bitcoin.Api.Client                   as Btc
 import qualified Network.Bitcoin.Api.Misc                     as Btc
 import qualified Network.Bitcoin.Api.Transaction              as Btc
 import           Network.Bitcoin.Api.Types.UnspentTransaction (amount)
 import qualified Network.Bitcoin.Api.Wallet                   as Btc
+
+import           Debug.Trace                                  (trace)
 
 data Availability =
   Available |
@@ -65,10 +75,10 @@ isAvailable host port user pass =
 advertise :: MonadIO m
           => Btc.Client          -- ^ Our client session
           -> Btc.Btc             -- ^ The fee we wish to pay to miners
-          -> BS.ByteString       -- ^ The message to send, might be spread over
-                                 --   multiple lines.
+          -> BS.ByteString       -- ^ The message header
+          -> BS.ByteString       -- ^ The message to send
           -> m Btc.TransactionId -- ^ The transaction that was sent to the network
-advertise client fee message =
+advertise client fee header message =
   let unspentBtc =
         foldr ((+) . (^. amount)) 0
 
@@ -82,7 +92,7 @@ advertise client fee message =
         tx & Btc.txOut <>~ outTxs
 
         where
-          outTxs               = [scriptify message]
+          outTxs               = [scriptify (header <> message)]
           scriptify msg        = Btc.TransactionOut 0 (Btc.Script [Btc.OP_RETURN, Btc.OP_PUSHDATA msg Btc.OPCODE])
 
   in liftIO $ do
@@ -102,8 +112,27 @@ advertise client fee message =
 
     Btc.send client tx'
 
-discover :: MonadIO m
-         => Btc.Client    -- ^ Our client session
-         -> BS.ByteString -- ^ Header that we are looking for
-         -> m ()          -- ^ Conduit source which only contains only matching Transactions
-discover = undefined
+-- | This is the consuming counterpart of 'advertise', and it will return all
+--   the messages sent, without the header.
+discover :: Btc.Client                -- ^ Our client session
+         -> BS.ByteString             -- ^ Header that we are looking for
+         -> C.Source IO BS.ByteString -- ^ Conduit which only contains only
+                                      --   messages that are being advertised.
+discover client header =
+  let predicate = isJust   . maybeExtract
+      extract   = fromJust . maybeExtract
+
+      -- Extracts message from transaction in case the message starts with
+      -- 'header'. The header is stripped from the message that is returned.
+      maybeExtract :: Btc.Transaction -> Maybe BS.ByteString
+      maybeExtract (Btc.Transaction _ _ outputs _) = go outputs
+        where
+          go [] = Nothing
+          go ((Btc.TransactionOut _ (Btc.Script [Btc.OP_RETURN, Btc.OP_PUSHDATA msg Btc.OPCODE])):xs) =
+            case BS.findSubstring header msg of
+             Just 0 -> (Just (BS.drop (BS.length header) msg))
+             _      -> go xs
+
+          go (_:xs) = go xs
+
+  in (Btc.watch client Nothing) $= (CC.filter predicate) $= (CC.map extract)
--- bitgloom-btc/test/Bitgloom/BTCSpec.hs
+++ bitgloom-btc/test/Bitgloom/BTCSpec.hs
@@ -2,18 +2,26 @@
 
 module Bitgloom.BTCSpec where
 
+import Control.Concurrent.MVar (newEmptyMVar, takeMVar, putMVar)
+import Control.Concurrent (forkIO, threadDelay)
+
+import qualified Data.Conduit as C (await)
+import Data.Conduit (($$))
+
 import qualified Data.Base32String.Default  as B32S (fromText, toBytes)
 import qualified Data.ByteString.Char8      as BS8
 import qualified Data.Text                  as T
 import qualified Data.Text.Encoding         as TE
 import qualified Data.HexString             as HS
-import Data.Monoid ((<>))
 
 import qualified Network.Bitcoin.Api.Client as Btc
 import qualified Network.Bitcoin.Api.Mining as Btc
 
 import Data.Word (Word16)
-import           Bitgloom.BTC
+import           Bitgloom.BTC (  Availability (..)
+                               , isAvailable
+                               , advertise
+                               , discover )
 import           Test.Hspec
 
 spec :: Spec
@@ -31,13 +39,24 @@ spec = do
       Btc.withClient "127.0.0.1" 18332 "user" "pass" $ \client -> do
 
         -- Let's flush any transactions so we know for sure we have money to spend
-        _   <- Btc.generate client 1
-        txid <- advertise client 0.0001 message
-        putStrLn ("txid = " ++ show txid)
-        True `shouldBe` True
+        _   <- Btc.generate client 10
+
+        -- Now broadcast the actual message we want to detect.
+        txid <- advertise client 0.0001 header onionAddress
+
+        result <- newEmptyMVar
+
+        _ <- forkIO $ do
+          -- Waits for a single return value
+          putMVar result =<< ((discover client header) $$ C.await)
+
+        -- Generate enough blocks for the 'discover' function to see the block
+        -- that contains the transaction.
+        threadDelay 1000000
+        _   <- Btc.generate client 10
+
+        takeMVar result `shouldReturn` (Just onionAddress)
 
         where
           header       = HS.toBytes $ HS.hexString "b76a"
           onionAddress = B32S.toBytes $ B32S.fromText "6MQF77PTMIV3BL6S"
-
-          message      = header <> onionAddress
